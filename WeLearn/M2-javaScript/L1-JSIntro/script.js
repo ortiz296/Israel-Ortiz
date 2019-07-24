988ba851c18207272ef7294a413b567dcfff5d5c
Israel Ortiz <ortiz296@csusm.edu>
2019-07-24 11:59:54 -0700

    Java1

---
+++ WeLearn/M2-javaScript/L1-JSIntro/script.js
@@ -0,0 +1,16 @@
+console.log("Script Running!");
+
+let name = 'Charlie';
+console.log('Hello ' + name + '!');
+let age = 15;
+console.log('You are ' + age + ' years old!');
+
+if (age >= 16) {
+  console.log('I can drive')
+} else {
+  console.log('I cannot drive')
+}
+
+if (age < 18 && name === "John"){
+  
+}
