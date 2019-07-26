b96074081e886ee8fca4b78d7f3f042a7e857ea4
Israel Ortiz <ortiz296@csusm.edu>
2019-07-26 12:02:57 -0700

    ?

---
+++ WeLearn/M3-Python/L1-Python_Intro/hello.py
@@ -0,0 +1,45 @@
+'''num1 = int(raw_input("Enter number #1: "))
+num2 = int(raw_input("Enter number #2: "))
+total = num1 + num2
+print("The sum is " + str(total))
+num = int(raw_input("Enter a number: "))
+if num > 0:
+    print("That's a positive number!")
+elif num < 0: # Note that we use the one-word eliso)
+    print("That's a negative number!")
+else:
+    print("Zero os neither positive nor negative")'''
+
+
+
+'''string = "hello there"
+for letter in string:
+    print(letter.upper())
+for i in range(11):
+    print(i)
+x = 1
+while x <= 5:
+    print(x)
+    x = x + 1'''
+
+
+'''my_name = "Bob"
+friend1 = "Alice"
+friend2 = "John"
+friend3 = "Mallory"
+
+print(
+"My name is %s and my friends are %s, %s, and %s" %
+(my_name, friend1, friend2, friend3)
+)'''
+
+
+'''def greetAgent():
+    print("Bond. James Bond.")
+
+def greetAgent(first_name, last_name):
+    print("%s. %s %s." % (last_name, first_name, last_name))
+greetAgent('James', 'Bond')
+
+def createAgentGreeting(first_name, Last_name):
+    return "&s. %s %s." % (last_name, first_name, last_names)'''
---
+++ WeLearn/M3-Python/L1-Python_Intro/mysum.py
@@ -0,0 +1,7 @@
+def findSum(x):
+    sum = 0
+    for i in range(x):
+        sum = sum + i
+    return sum+x
+
+print(findSum(20))
